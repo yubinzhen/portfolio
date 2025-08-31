48bf4675d0998ae04b4162296a81206ca2095586
Yubin Zhen <yubinzhen020105@gmail.com>
2025-08-31 15:44:30 -0700

    404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,9 @@
 const NotFound = () => {
-  return <div>404 Page Not Found</div>;
+  return (
+    <div className="text-center text-3xl font-bold text-red-500">
+      404: Page Cannot Be Found
+    </div>
+  );
 };
 
 export default NotFound;
